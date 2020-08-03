5a2158244a0203989a4abed0fdf13b52de19537a
shenyaqi <shenyaqiii@163.com>
2020-08-03 11:44:28 +0800

    高可用config-server-v1.0

--- spring-cloud-parent/config-server/src/main/java/com/mr/syq/ConfigServerStart.java
+++ spring-cloud-parent/config-server/src/main/java/com/mr/syq/ConfigServerStart.java
@@ -3,6 +3,7 @@ package com.mr.syq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ClassName ConfigServerStart
@@ -11,8 +12,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @Date 2020/7/27
  * @Version V1.0
  **/
+
 @SpringBootApplication
 @EnableConfigServer//声明当前服务是一个configserver
+@EnableEurekaClient//将当前服务注册到eureka服务中,高可用
 public class ConfigServerStart {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerStart.class);
