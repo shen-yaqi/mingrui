45cb7226624e7da3d007b100bcb7a9c8625a6fb8
shenyaqi <shenyaqiii@163.com>
2020-08-03 17:31:02 +0800

    bus消息总线

--- spring-cloud-parent/eureka-client/src/main/java/com/mr/syq/controller/TestConfigController.java
+++ spring-cloud-parent/eureka-client/src/main/java/com/mr/syq/controller/TestConfigController.java
@@ -1,6 +1,7 @@
 package com.mr.syq.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping(value = "config-client-controller")
+@RefreshScope//动态刷新配置
 public class TestConfigController {
 
     @Value(value = "${mingrui.url}")
